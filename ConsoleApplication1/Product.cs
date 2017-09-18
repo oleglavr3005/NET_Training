64a30eb54b6071f7f70238e47a0507f071034e7a
olavr <olavr@LEGO>
2017-09-18 17:57:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Product : ICloneable, IComparable
+{
+    public string ProductName { get; set; }
+    public string ProductId { get; set; }
+    public int ProductCode { get; set; }
+    public decimal ProductPrice { get; set; }
+
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+
+
+    public int CompareTo(Object obj)
+    {
+        return ProductCode - ((Product)obj).ProductCode;
+    }
+
+    public Product(string name, string id, int code, decimal price)
+    {
+        ProductName = name;
+        ProductId = id;
+        ProductCode = code;
+        ProductPrice = price;
+    }
+
+    public override string ToString()
+    {
+        return ProductName + "  " + ProductId;
+    }
+}
+
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            { new Product("1", "1", 1013, 81.67m),
+             new Product("2", "2", 1017, 49.99m) ,
+            new Product("3", "3", 1015, 49.00m) }
+            ;
+            products.Sort();
+            foreach (Product prod in products) Console.WriteLine(prod);
+            Console.ReadLine();
+        }
+    }
+}
