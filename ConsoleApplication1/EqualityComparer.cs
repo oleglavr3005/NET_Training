faa926494947f7e327afd933c503338c441b0cce
olavr <olavr@LEGO>
2017-09-21 12:01:59 +0300

    Refactoring of EqualityComparer

--- ConsoleApplication1/EqualityComparer.cs
+++ ConsoleApplication1/EqualityComparer.cs
@@ -11,15 +11,12 @@ namespace ConsoleApplication1
     class EqualityComparer : IEqualityComparer<System.Object>
     {
         public static readonly ILog log = LogManager.GetLogger(typeof(EqualityComparer));
-        public new bool Equals(object x, object y)
+
+        private List<FieldInfo> GetFieldsForEqualityCheck(object x)
         {
-            //if different classes return false
-            //if the same class get attributes values and check them for equals
-            if (x == null || y == null || x.GetType() != y.GetType())
-                return false;
-         //   x.GetType().GetCustomAttributes(false);
+            List<FieldInfo> listOfFields = new List<FieldInfo>();
             UseForEqualityCheck MyAttribute =
-           (UseForEqualityCheck)Attribute.GetCustomAttribute(x.GetType(), typeof(UseForEqualityCheck));
+         (UseForEqualityCheck)Attribute.GetCustomAttribute(x.GetType(), typeof(UseForEqualityCheck));
 
             if (MyAttribute == null)
             {
@@ -29,35 +26,44 @@ namespace ConsoleApplication1
             {
                 foreach (string UseFieldForCheckEquals in MyAttribute.FieldsForEqualityCheck)
                 {
-                    FieldInfo[] fi = x.GetType().GetFields(BindingFlags.Public |
+                    listOfFields.Add(x.GetType().GetField(UseFieldForCheckEquals, BindingFlags.Public |
                                                  BindingFlags.NonPublic |
-                                                 BindingFlags.Instance);
-                    FieldInfo x_field = x.GetType().GetField(UseFieldForCheckEquals, BindingFlags.Public |
-                                                 BindingFlags.NonPublic |
-                                                 BindingFlags.Instance);
-                    FieldInfo y_field = y.GetType().GetField(UseFieldForCheckEquals, BindingFlags.Public |
-                                                  BindingFlags.NonPublic |
-                                                  BindingFlags.Instance);
-                    //        object ox = (ObjectType)Activator.CreateInstance(); oy = new object();
-                    Type type = x_field.GetType();
-                    object ox = x_field.GetValue(x);
-                    object oy = y_field.GetValue(y);
-                    if (!ox.Equals(oy))
-                    {
-                        log.Info("false");
-                        return false;
-                    }
+                                                 BindingFlags.Instance));
                 }
-
             }
+            return listOfFields;
+        }
+        public new bool Equals(object x, object y)
+        {
+            //if different classes return false
+            //if the same class get attributes values and check them for equals
+            if (x == null || y == null || x.GetType() != y.GetType())
+                return false;
+            //   x.GetType().GetCustomAttributes(false);
+            foreach (FieldInfo checkedField in GetFieldsForEqualityCheck(x))
+            {
+                object ox = checkedField.GetValue(x);
+                object oy = checkedField.GetValue(y);
+                if (!ox.Equals(oy))
+                {
+                    log.Info("false");
+                    return false;
+                }
+            }
+
 
             return true;
         }
 
-        public int GetHashCode(object obj)
+        public int GetHashCode(object x)
         {
-            return 0;
-            throw new NotImplementedException();
+            int hash = 0;
+            foreach (FieldInfo checkedField in GetFieldsForEqualityCheck(x))
+            {
+                object ox = checkedField.GetValue(x);
+                hash += ox.GetType().GetHashCode();
+            }
+            return hash;
         }
     }
 }
