14a2ef77eeb3a4c76026161ff8e406c5ab313e8a
olavr <olavr@LEGO>
2017-09-25 13:24:40 +0300

    small fixes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -16,43 +16,44 @@ namespace ConsoleApplication1
             log4net.Config.XmlConfigurator.Configure();
             log.Info("Start application.");
 
-            Product updProduct=null;
-
+            Product updProduct = null;
+            List<Product> updProducts = new List<Product>();
             List<Product> products = new List<Product>()
             { new Product("Prod1", 145445, 1013, 81.67m),
-             new Product("Prod2", 435435436, 1017, 49.99m) ,
+              new Product("Prod2", 435435436, 1017, 49.99m) ,
               new Product("Prod2", 435435435, 1044, 59.99m) ,
-            new Product("Prod3", 354554, 1015, 49.00m) ,
-            new Product("Prod3", 354554, 1015, 89.00m) ,
-            new Product("Prod3", 354554, 1015, 49.00m) }
+              new Product("Prod3", 354554, 1015, 49.00m) ,
+              new Product("Prod3", 354554, 1015, 89.00m) ,
+              new Product("Prod3", 354554, 1015, 49.00m) }
             ;
-            products= products.Distinct<Product>(new EqualityComparer()).ToList<Product>();
+            products = products.Distinct<Product>(new EqualityComparer()).ToList<Product>();
             products.Sort();
             using (var ctx = new ProductsContext())
             {
-                /*    foreach (Product prod in products)
-                    {
-                        Console.WriteLine(prod);
-                        ctx.Products.Add(prod);
+                foreach (Product prod in products)
+                {
+                    Console.WriteLine(prod);
+                    ctx.Products.Add(prod);
 
-                    }
-                    */
-                updProduct= ctx.Products.Where(s => s.ProductId == 354554).FirstOrDefault<Product>();
-                if (updProduct != null)
-                    updProduct.ProductPrice = 59.00m;
+                }
+                updProducts = ctx.Products.Where(s => s.ProductName == "Prod3").ToList<Product>();
+                foreach (Product product in updProducts)
+                    product.ProductPrice = 99.00m;
                 ctx.SaveChanges();
 
 
             }
-            using (var dbCtx = new ProductsContext())
-            {
-                //3. Mark entity as modified
-                dbCtx.Entry(updProduct).State = System.Data.Entity.EntityState.Modified;
-
-                //4. call SaveChanges
-                dbCtx.SaveChanges();
-            }
-
+            /*       using (var dbCtx = new ProductsContext())
+                   {
+                       //3. Mark entity as modified
+                  dbCtx.Entry(updProduct).State = System.Data.Entity.EntityState.Modified;
+                       updProduct = dbCtx.Products.Where(s => s.ProductName == "Prod3").First<Product>();
+                       if (updProduct != null)
+                           updProduct.ProductPrice = 59.00m;
+                       //4. call SaveChanges
+                       dbCtx.SaveChanges();
+                   }
+       */
             Console.ReadLine();
 
             log.Info("End application.");
